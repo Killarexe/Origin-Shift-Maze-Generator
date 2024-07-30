a3a22dd825c5a8b14da383ffc3b3f4451647377d
Killar.exe <killar.exe@gmail.com>
2024-07-30 08:34:27 +0200

    Draw basic maze

--- sources/maze.c
+++ sources/maze.c
@@ -6,7 +6,6 @@
 
 Maze* create_maze(int width, int height) {
   Maze* maze = malloc(sizeof(struct Maze));
-  
   int maze_size = width * height;
   int* maze_data = malloc(sizeof(int) * maze_size);
   maze_data[0] = 0;
@@ -25,19 +24,32 @@ Maze* create_maze(int width, int height) {
   return maze;
 }
 
+int get_node(Maze* maze, int x, int y) {
+  int index = y * maze->width + x;
+  if (index < (maze->width * maze->height)) {
+    return maze->maze_data[index];
+  }
+  return 0;
+}
 
 void draw_maze(Maze *maze, int size) {
   int maze_size = maze->width * maze->height;
   for(int i = 0; i < maze_size; i++) {
-    int x = (i % maze->width) * size;
-    int y = floor((double)i / (double)maze->width) * size;
-    int cell = maze->maze_data[i];
-    for (int x_offset = -1; x_offset < -2; x_offset++) {
-      for (int y_offset = -1; y_offset < -2; y_offset++) {
-        if (x_offset != y_offset && x_offset != 0) {
-          int n_cell = maze->maze_data[i];
-        }
+    int x_node = (i % maze->width);
+    int y_node = floor((double)i / (double)maze->width);
+    int x_pos = x_node * size;
+    int y_pos = y_node * size;
+    int node = maze->maze_data[i];
+    if (node == 0x2) {
+      if (get_node(maze, x_node - 1, y_node) != 0x4) {
+        DrawLine(x_pos, y_pos, x_pos, y_pos + size, WHITE);
       }
+      if (get_node(maze, x_node + 1, y_node) != 0x8) {
+        DrawLine(x_pos + size, y_pos, x_pos + size, y_pos + size, WHITE);
+      }
+    } else if (node == 0x8) {
+      DrawLine(x_pos, y_pos + size, x_pos + size, y_pos + size, WHITE);
+      DrawLine(x_pos, y_pos, x_pos + size, y_pos, WHITE);
     }
   }
 }
--- sources/maze.h
+++ sources/maze.h
@@ -1,7 +1,6 @@
 #ifndef MAZE_H
 #define MAZE_H
 
-
 /*
  * Maze data format:
  * The maze data represents each cells(int value) as a 4-bit number,
@@ -15,6 +14,11 @@
  *
  * Exemple: 0b1010 This is a cell width the direction Left and Up.
  */
+
+#define IS_LEFT(x) x & 0x8 == 1
+#define IS_RIGHT(x) x & 0x4 == 1
+#define IS_UP(x) x & 0x2 == 1
+#define IS_DOWN(x) x & 0x1 == 1
 
 typedef struct Maze {
   int* maze_data;
